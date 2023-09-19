d09bb5a6416905031f94394d31efb76d3d4548e8
SEONG JUN SHIN <shinsj4653@konkuk.ac.kr>
2023-09-19 10:47:55 +0900

    feat: ci/cd test 2

--- src/main/java/visang/showcase/aibackend/AibackendApplication.java
+++ src/main/java/visang/showcase/aibackend/AibackendApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AibackendApplication {
 
-    // ci cd test 1
+    // ci cd test 2
     public static void main(String[] args) {
         SpringApplication.run(AibackendApplication.class, args);
     }
