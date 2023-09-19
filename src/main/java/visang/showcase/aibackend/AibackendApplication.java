56deaab821c23c864e3d9801a92e9ca813458dc0
SEONG JUN SHIN <shinsj4653@konkuk.ac.kr>
2023-09-19 09:37:00 +0900

    first commit

---
+++ src/main/java/visang/showcase/aibackend/AibackendApplication.java
@@ -0,0 +1,14 @@
+package visang.showcase.aibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AibackendApplication {
+
+    // ci cd test 3
+    public static void main(String[] args) {
+        SpringApplication.run(AibackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/visang/showcase/aibackend/AibackendApplicationTests.java
@@ -0,0 +1,13 @@
+package visang.showcase.aibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AibackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
