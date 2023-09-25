2ba2000a9a4a98e42b1f3295e511d2da15ff1836
SEONG JUN SHIN <shinsj4653@konkuk.ac.kr>
2023-09-25 11:20:10 +0900

    feat: 학생의 문제 풀이 시퀀스 조회 query문 작성

--- src/main/java/visang/showcase/aibackend/dto/response/diagnosis/DiagnosisProblemDto.java
+++ src/main/java/visang/showcase/aibackend/dto/response/diagnosis/DiagnosisProblemDto.java
@@ -3,7 +3,7 @@ package visang.showcase.aibackend.dto.response.diagnosis;
 import lombok.Getter;
 
 @Getter
-public class MemberDiagnosisProblemDto {
+public class DiagnosisProblemDto {
     private String prob_no; // 문항 번호
     private Integer prob_seq_no; // 문항 시퀀스 번호
     private String topic_nm; // 토픽 한글명
