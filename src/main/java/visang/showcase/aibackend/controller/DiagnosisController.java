2354956878e652d8a596e6f7b73728e0cc0c3a08
SEONG JUN SHIN <shinsj4653@konkuk.ac.kr>
2023-09-25 11:37:08 +0900

    feat: HttpSession에서 memberNo 값을 반환하여 서비스 단으로 전달

--- src/main/java/visang/showcase/aibackend/controller/DiagnosisController.java
+++ src/main/java/visang/showcase/aibackend/controller/DiagnosisController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 import visang.showcase.aibackend.dto.response.common.ResponseDto;
 import visang.showcase.aibackend.dto.response.common.ResponseUtil;
 import visang.showcase.aibackend.dto.response.diagnosis.DiagnosisProblemDto;
-import visang.showcase.aibackend.dto.response.member.MemberDto;
 import visang.showcase.aibackend.service.DiagnosisService;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
@@ -19,7 +19,15 @@ public class DiagnosisController {
 
     private final DiagnosisService diagnosisService;
     @GetMapping
-    public ResponseDto<List<DiagnosisProblemDto>> getProblems() {
-        return ResponseUtil.SUCCESS("타켓 학생의 문제 풀이 시퀀스, 응답 시퀀스 조회 성공 ", diagnosisService.getProblems());
+    public ResponseDto<List<DiagnosisProblemDto>> getProblems(HttpSession session) {
+        String memberNo = (String) session.getAttribute("memberNo");
+        
+        // memberNo 값이 세션에 존재할 경우에만 서비스단 로직 수행
+        if (memberNo != null) {
+            List<DiagnosisProblemDto> problems = diagnosisService.getProblems(memberNo);
+            return ResponseUtil.SUCCESS("타켓 학생의 문제 풀이 시퀀스, 응답 시퀀스 조회 성공 ", problems);
+        } else {
+            return ResponseUtil.ERROR("세션에 memberNo가 없습니다.", null);
+        }
     }
 }
