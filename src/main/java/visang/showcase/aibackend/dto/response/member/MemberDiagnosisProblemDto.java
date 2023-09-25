cb1e0f4d7e63a30896947bc36560d054008c0fdf
SEONG JUN SHIN <shinsj4653@konkuk.ac.kr>
2023-09-25 11:10:37 +0900

    feat: 진단평가에 필요한 학생 문제 시퀀스 dto 생성

---
+++ src/main/java/visang/showcase/aibackend/dto/response/member/MemberDiagnosisProblemDto.java
@@ -0,0 +1,14 @@
+package visang.showcase.aibackend.dto.response.member;
+
+import lombok.Getter;
+
+@Getter
+public class MemberDiagnosisProblemDto {
+    private String prob_no; // 문항 번호
+    private Integer prob_seq_no; // 문항 시퀀스 번호
+    private String topic_nm; // 토픽 한글명
+    private String chapter_nm;  // 대단원 한글명
+    private String section_nm; // 중단원 한글명
+    private String subsection_nm; // 소단원 한글명
+    private Integer correct; // 문항 정오답 여부
+}
