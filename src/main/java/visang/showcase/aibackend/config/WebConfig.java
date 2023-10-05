82c26f377eee67ba04658792abb2041c05f2e867
vs-dp-kimdw3 <kimdw3@visang.com>
2023-10-05 16:24:28 +0900

    2023.10.05 cors 해결 테스트

--- src/main/java/visang/showcase/aibackend/config/WebConfig.java
+++ src/main/java/visang/showcase/aibackend/config/WebConfig.java
@@ -2,17 +2,18 @@ package visang.showcase.aibackend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*") //로컬, 개발
                 .allowedHeaders("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-                .allowCredentials(true) // 쿠키 허용
-                .maxAge(3600); // preflight(사전 요청)의 캐시 시간 설정
+                .allowedMethods("*"); // 허용할 HTTP 메서드
+
     }
 }
